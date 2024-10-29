f1cf93ede5831d332856e5f97c1890ac13c42b1b
Matheus Ferreira <matheusfer0902@gmail.com>
2024-10-29 15:47:56 -0300

    Estruturação da pagina home

--- src/app/components/homeSection.tsx
+++ src/app/components/homeSection.tsx
@@ -4,54 +4,50 @@ import { ArrowUpRight } from 'lucide-react';
 const HomeSection = () => {
   return (
     <div className="bg-purple-600 h-screen flex flex-col justify-center items-center p-4">
-      <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold text-white mb-4">LOGO</h1>
-        <p className="text-white">
-          Somos a Acelera! Uma empresa que desenvolve soluções Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+      <div className="text-center mb-8 max-w-2xl">
+        <h1 className="text-5xl font-extrabold text-white mb-4">LOGO</h1>
+        <p className="text-white text-lg mb-2">
+          Somos a Acelera! Uma empresa que desenvolve soluções inovadoras para transformar o seu negócio.
         </p>
-        <p className="text-white">
-          Confira as nossas demos!
+        <p className="text-white text-lg font-light pb-7">
+          Confira as nossas demos e explore o que podemos oferecer!
         </p>
       </div>
-      <div className="flex space-x-4">
-        <div className='border border-white-2 rounded-lg p-4'>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-4xl w-full">
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
           <a
             href="https://drive.google.com/file/d/1DFEBd7YK4crrdvOZWof5M5FZMo_0284o/view?usp=sharing"
-            className="bg-white text-purple-600 font-semibold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-cyan-400 hover:text-white flex items-center"
+            className="bg-purple-600 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-white hover:text-purple-600 hover:border hover:border-purple-600 font-semibold text-lg mb-4 flex items-center"
           >
             Atendente por voz
             <ArrowUpRight className="ml-2" />
           </a>
-          <p className='pt-2'>Atendente de voz personalizado para cada ocasião do seu negócio. 
-            lorem Lorem lorem lorem Atendente de voz personalizado para cada ocasião do seu negócio. 
-            lorem Lorem lorem lorem Atendente de voz personalizado para cada ocasião do seu negócio. 
-            lorem Lorem lorem lorem
+          <p className="text-gray-700">
+            Atendente de voz personalizado para cada ocasião do seu negócio, oferecendo uma experiência mais próxima e eficiente.
           </p>
         </div>
-        <div className='border border-white-2 rounded-lg p-4'>
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
           <a
             href="https://drive.google.com/file/d/18uQQXJfeO5MzSB8a8LLzco0KQ-t237kp/view?usp=sharing"
-            className="bg-white text-purple-600 font-semibold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-cyan-400 hover:text-white flex items-center"
+            className="bg-purple-600 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-white hover:text-purple-600 hover:border hover:border-purple-600 font-semibold text-lg mb-4 flex items-center"
           >
             Gestor inteligente
             <ArrowUpRight className="ml-2" />
           </a>
-          <p className='pt-2'>Gestor inteligente, auxilia na administração geral da sua empresa. 
-            Seja a área financeira, como porcentagem de vendas, faturamento. 
-            O gestor funciona tanto com texto, como por voz!
+          <p className="text-gray-700">
+            Auxilia na administração geral da sua empresa, oferecendo suporte nas áreas financeira e operacional, seja por texto ou por voz.
           </p>
         </div>
-        <div className='border border-white-2 rounded-lg p-4'>
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
           <a
             href="https://drive.google.com/file/d/1LedJXzVlRjBJ-puujQrFLsXq7sLGmo3j/view?usp=sharing"
-            className="bg-white text-purple-600 font-semibold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-cyan-400 hover:text-white flex items-center"
+            className="bg-purple-600 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-white hover:text-purple-600 hover:border hover:border-purple-600 font-semibold text-lg mb-4 flex items-center"
           >
             Plataforma de vendas
             <ArrowUpRight className="ml-2" />
           </a>
-          <p className='pt-2'>Plataforma de gerenciamento de chatbots com IA. 
-            Plataforma completa para o gerenciamento e monitoramento de vendas. 
-            Uma IA ira supervisionar chatbots para as vendas
+          <p className="text-gray-700">
+            Plataforma completa para o gerenciamento e monitoramento de vendas, supervisionada por uma IA inteligente.
           </p>
         </div>
       </div>
