a2322969e2c2ad96d3abe960b0ce57da9c502983
Matheus Ferreira <matheusfer0902@gmail.com>
2024-10-31 16:34:42 -0300

    Arrumando as imagens

--- src/app/components/homeSection.tsx
+++ src/app/components/homeSection.tsx
@@ -9,12 +9,12 @@ const HomeSection = () => {
         <p className="text-white text-lg mb-2">
           Somos a Acelera! Uma empresa que desenvolve soluções inovadoras para transformar o seu negócio.
         </p>
-        <p className="text-white text-lg font-light pb-7">
+        <p className="text-white text-lg font-light">
           Confira os nossos produtos e explore o que podemos oferecer!
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-2xl w-full">
-        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center h-65">
           <a
             href="https://drive.google.com/file/d/1DFEBd7YK4crrdvOZWof5M5FZMo_0284o/view?usp=sharing"
             className="bg-red-600 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-white hover:text-red-600 hover:border hover:border-red-600 font-semibold text-lg mb-4 flex items-center"
@@ -23,10 +23,11 @@ const HomeSection = () => {
             <ArrowUpRight className="ml-2" />
           </a>
           <p className="text-gray-700">
-            Atendente de voz personalizado para cada ocasião do seu negócio, oferecendo uma experiência mais próxima e eficiente.
+            Chatbot personalizado para cada ocasião do seu negócio.
           </p>
+          <img src='/chatbot.png' className='h-40 w-40'></img>
         </div>
-        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center text-center h-65">
           <a
             href="https://drive.google.com/file/d/18uQQXJfeO5MzSB8a8LLzco0KQ-t237kp/view?usp=sharing"
             className="bg-red-600 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-white hover:text-red-600 hover:border hover:border-red-600 font-semibold text-lg mb-4 flex items-center"
@@ -35,8 +36,9 @@ const HomeSection = () => {
             <ArrowUpRight className="ml-2" />
           </a>
           <p className="text-gray-700">
-            Auxilia na administração geral da sua empresa, oferecendo suporte nas áreas financeira e operacional, seja por texto ou por voz.
+            Auxilia na sua preparação para cursos e concursos.
           </p>
+          <img src='/tutor.jpg' className='h-40 w-30'></img>
         </div>
       </div>
     </div>
